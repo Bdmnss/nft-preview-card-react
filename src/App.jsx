66a246ebd377400e4f3f01a4da5907e6b86234da
Bdmnss <lukabadali123@gmail.com>
2024-06-06 20:12:56 +0400

    adding styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,22 @@ function App() {
   return (
     <>
       <div className="card">
-        <img src={HeroImg} alt="equilibrium image" />
+        <img src={HeroImg} alt="equilibrium image" className="hero-img" />
         <div className="text-div">
-          <p>Equilibrium #3429</p>
+          <h1>Equilibrium #3429</h1>
           <p>Our Equilibrium collection promotes balance and calm.</p>
         </div>
-        <div className="ethereum-div">
-          <div>
+        <div className="ethereum-options-div">
+          <div className="ethereum-div">
             <img src={EthereumIcon} alt="ethereum icon" />
             <p>0.041 ETH</p>
           </div>
-          <div>
+          <div className="clock-div">
             <img src={ClockIcon} alt="clock icon" />
             <p>3 days left</p>
           </div>
         </div>
-        <div className="creator div">
+        <div className="creator-div">
           <img src={AvatarImg} alt="avatar" />
           <p>Creation of <span>Jules Wyvern</span></p>
         </div>
